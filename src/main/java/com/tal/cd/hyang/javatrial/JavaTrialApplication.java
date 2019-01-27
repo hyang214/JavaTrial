dd9531f1478c632108176ffb24511b8e7cba284e
Hao YANG <>
2019-01-27 11:10:08 +0800

    empty project

---
+++ src/main/java/com/tal/cd/hyang/javatrial/JavaTrialApplication.java
@@ -0,0 +1,18 @@
+package com.tal.cd.hyang.javatrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrialApplication {
+    
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(JavaTrialApplication.class, args);
+        } catch (Exeception e) {
+
+        }
+    }
+
+}
+
